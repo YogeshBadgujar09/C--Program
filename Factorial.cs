7d204aa22258f5c8f656ece8bc19711db53e4d1b
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-08 20:12:56 -0800

    [ADDED] : YOGESH Factorial Number Program

---
+++ Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigment2
+{
+    class Factorial
+    { 
+        public static void Main(string[] args)
+        {
+            int num,fact=1 ,i=1;
+
+            System.Console.Write("Enter a number :");
+            num = Convert.ToInt16(System.Console.ReadLine());
+
+            do
+            {
+                fact = fact * i;
+
+                i++;
+            }
+            while (i <= num);
+
+            System.Console.WriteLine("Factorial of "+num+" is "+fact);
+
+            System.Console.Read();
+        }
+    }
+}
