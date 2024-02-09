b09bb4cc56774bcaee851956209f56c52f58bf62
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-08 20:10:40 -0800

    [ADDED] : YOGESH Number is Even Or Odd

---
+++ Even_Odd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigment2
+{
+    class Even_Odd
+    {
+        static void Main(string[] args) 
+        {
+            System.Console.WriteLine("Programe For Number Is Even Or odd \n");
+
+            int a;
+
+            System.Console.Write("Enter a number :");
+            a = Convert.ToInt16(System.Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                System.Console.Write(a + " is Even Number");
+            }
+            else {
+                System.Console.Write(a + " is Odd Number");
+            }
+
+            System.Console.Read();
+        }
+    }
+}
