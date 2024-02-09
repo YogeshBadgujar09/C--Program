7b8086952b89db9a20ebf195201f16d5524c52f3
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-08 20:26:58 -0800

    [ADDED] : YOGESH Simple Mathematical Operation

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Program for Mathematicals Operation \n");
+
+            int a, b, c;
+
+            System.Console.Write("Enter first number :");
+            a = Convert.ToInt16(System.Console.ReadLine());
+
+            System.Console.Write("Enter first number :");
+            b = Convert.ToInt16(System.Console.ReadLine());
+
+            c = a + b;
+            System.Console.WriteLine("Addition :"+c);
+
+            c = a - b;
+            System.Console.WriteLine("Subtraction :" + c);
+
+            c = a * b;
+            System.Console.WriteLine("Multiplication :" + c);
+
+            c = a / b;
+            System.Console.WriteLine("Division :" + c);
+
+
+            c = a % b;
+            System.Console.WriteLine("Module :" + c);
+
+            System.Console.Read();
+        }
+    }
+}
