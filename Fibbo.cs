7148cfe9ff3745d85a23e6ac48005eb0e021a501
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-08 20:14:55 -0800

    [ADDED] : YOGESH Program of Fibbonaci Series

---
+++ Fibbo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigment2
+{
+    class Fibbo
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            int p1 = 0, p2 = 1, temp;
+
+            System.Console.Write("Enter a number :");
+            n = Convert.ToInt16(System.Console.ReadLine());
+
+            System.Console.Write(p1 +" "+p2);
+
+            for (int i = 3 ;i <= n ;i++)
+            {
+                temp = p1 + p2;
+                p1 = p2;
+                p2 = temp;
+
+                System.Console.Write(" "+temp);
+            }
+
+            System.Console.Read();
+        }
+       
+    }
+}
