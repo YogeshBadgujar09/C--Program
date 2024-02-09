219742924919944dc4e1c4d8617036f501bb5152
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-08 20:29:35 -0800

    [ADDED] : YOGESH Reverse Number Program

---
+++ ReverseNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigment2
+{
+    class ReverseNum
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            int r_n;
+            int r;
+
+            System.Console.Write("Enter a number :");
+            n = Convert.ToInt16(Console.ReadLine());
+
+            while (n > 0)
+            {
+                r= n % 10;
+                r_n = r;
+                n = n / 10;         
+
+                System.Console.Write(r_n);
+            }
+
+
+
+            System.Console.Read();
+
+        }
+    }
+}
