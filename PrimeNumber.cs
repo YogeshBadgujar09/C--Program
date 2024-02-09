4e9770ea5b9b102859d8791fbd0ad58fcdd87ee3
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-08 20:24:20 -0800

    [ADDED] : YOGESH Prime Number Program

---
+++ PrimeNumber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigment2
+{
+    class PrimeNumber
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+
+            int flag=1;
+
+            System.Console.Write("Enter a number :");
+            n = Convert.ToInt16(Console.ReadLine());
+
+            if (n == 1)
+            {
+                flag = 1;
+            }
+            else 
+            {
+                for (int i = 2; i < n; i++)
+                { 
+                    if(n%i==0)
+                    {
+                        flag = 0;
+
+                        break;
+                    }
+                }
+            }
+
+            if (flag == 1)
+            {
+                System.Console.Write("Number is Prime");
+            }
+            else {
+                System.Console.Write("Number is Not Prime");
+            }
+            
+         
+                System.Console.Read();
+
+        }
+    }
+}
