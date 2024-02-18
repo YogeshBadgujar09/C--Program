3b92d349ec444c28e4f1e87684142a5566f6a953
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-18 23:37:16 +0530

    [ADDED] : YOGESH Simple Boolean DataType Program

---
+++ BooleanDataType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BooleanDataType
+{
+    class BooleanDataType
+    {
+        static void Main(string[] args)
+        {
+            bool isValid = true;
+
+            Console.WriteLine(isValid);
+
+            Console.Read();
+
+        }
+    }
+}
