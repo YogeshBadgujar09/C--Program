64d463aef6c54844fbaed382151ed4a6ad0eebc4
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-18 23:45:44 +0530

    [ADDED] : YOGESH Assiment Operater Simple Program

---
+++ AssigmentOperater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeNumber
+{
+    class AssigmentOperater
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            a = 10;
+            Console.WriteLine("a=" + a);
+
+            b = a;
+            Console.WriteLine("b=" + b);
+
+            Console.Raed();
+        }
+    }
+}
