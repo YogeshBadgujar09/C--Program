b8152c13c2fd7db7a896c204e106565fe9c339f8
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-18 23:54:28 +0530

    [ADDED] : YOGESH ARithmatic Operater Program

---
+++ ArithmaticOperater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeNumber
+{
+    class ArithmaticOperater
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 15, b = 15;
+            int c;
+
+            c = a + b;
+            Console.WriteLine("ADD : " + c);
+
+            c = a - b;
+            Console.WriteLine("SUB : " + c);
+
+            c = a * b;
+            Console.WriteLine("MUL : " + c);
+
+            c = a / b;
+            Console.WriteLine("DIV : " + c);
+
+            c = a % b;
+            Console.WriteLine("MOD : " + c);
+
+            Console.Read();           
+        
+        }
+    }
+}
